24939601c8af138c1eaad6cbedc291383194481d
Dhabi966 <jalewis012003@yahoo.com>
2022-08-22 15:17:22 -0600

    changes to html, css and js

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,6 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
 
-    $("#hour9 .entries").val(localStorage.getItem("hour9"));
-    $("#hour10 .entries").val(localStorage.getItem("hour10"));
-    $("#hour11 .entries").val(localStorage.getItem("hour11"));
-    $("#hour12 .entries").val(localStorage.getItem("hour12"));
-    $("#hour13 .entries").val(localStorage.getItem("hour13"));
-    $("#hour14 .entries").val(localStorage.getItem("hour14"));
-    $("#hour15 .entries").val(localStorage.getItem("hour15"));
-    $("#hour16 .entries").val(localStorage.getItem("hour16"));
-    $("#hour17 .entries").val(localStorage.getItem("hour17"));
 
     function timeTracker() {
         var currentHour = moment().hour();
